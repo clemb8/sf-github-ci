38295d64b077a4b783ed244783227c4bd8de1794
clemb8 <clement@jb-p.fr>
2024-05-26 17:45:21 +0200

    mod

--- scripts/apex-coverage.sh
+++ scripts/apex-coverage.sh
@@ -1,5 +1,4 @@
 total=$(jq .total.lines.pct ./coverage/coverage/coverage-summary.json)
-echo "Total: $total"
 #display percentage
 if [ $total -ge 85 ]; then
     echo ":clap: Code coverage is $total% :clap:"
